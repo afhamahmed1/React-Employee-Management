1db0689489cdaa6a64a77a6a809f0559f689b348
Afham Ahmed <afham12ahmed@gmail.com>
2023-08-24 23:08:34 +0500

    ui changes in definition

--- src/pages/Definition.jsx
+++ src/pages/Definition.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { json, useNavigate, useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import { v4 } from "uuid";
 
 export default function Dictionary() {
@@ -16,34 +16,42 @@ export default function Dictionary() {
   return (
     <>
       <h1 className="text-xl capitalize font-bold">{search.search}</h1>
-      {searchResult.length
-        ? searchResult.map((meaning) => {
-            return (
-              <>
-                <ul key={v4()} className="list-decimal p-2 list-inside">
-                  {meaning.meanings.map((definitions) => {
-                    return (
-                      <>
-                        <h4 className="capitalize font-bold">
-                          {definitions.partOfSpeech}
-                        </h4>
-                        <ul>
-                          {definitions.definitions.map((def) => {
-                            return (
-                              <>
-                                <li key={v4()}>{def.definition}</li> <br />
-                              </>
-                            );
-                          })}
-                        </ul>
-                      </>
-                    );
-                  })}
-                </ul>
-              </>
-            );
-          })
-        : "No Record Found"}
+      {searchResult.length ? (
+        searchResult.map((meaning) => {
+          return (
+            <>
+              <ul key={v4()} className="list-decimal p-2 list-inside">
+                {meaning.meanings.map((definitions) => {
+                  return (
+                    <>
+                      <h4 className="capitalize font-bold">
+                        {definitions.partOfSpeech}
+                      </h4>
+                      <ul>
+                        {definitions.definitions.map((def) => {
+                          return (
+                            <>
+                              <li key={v4()}>{def.definition}</li> <br />
+                            </>
+                          );
+                        })}
+                      </ul>
+                    </>
+                  );
+                })}
+              </ul>
+            </>
+          );
+        })
+      ) : (
+        <>
+          <div className="p-2">
+            <h4 className="font-bold "> {searchResult.title}</h4>
+            <p className="px-2">{searchResult.message}</p>
+            <p className="px-2">{searchResult.resolution}</p>
+          </div>
+        </>
+      )}
     </>
   );
 }
